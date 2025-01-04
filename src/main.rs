dd99c853b94f90a5d3e538b9dcbfb9d3adf6dddd
Wojtaz <279730@student.pwr.edu.pl>
2025-01-04 13:51:24 +0100

    poprawa usera - dodanie funkcji user_exists i dostosowanie logiki

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,42 @@ use models::country::Country;
 fn main() -> Result<()> {
     let pool = connect_to_db()?;
     let mut conn = pool.get_conn()?;
+    let mut isEnd: bool = false;
+
+    while isEnd == false {
+        println!("1. Add user");
+        println!("2. Add city");
+        println!("3. Add country");
+        println!("4. Exit");
+        let mut choice = String::new();
+        std::io::stdin().read_line(&mut choice).expect("Failed to read line");
+        let choice: u32 = choice.trim().parse().expect("Please type a number!");
+
+        match choice {
+            1 => {
+                let user = User::new("admin", "admin", "active");
+                user.add_user(&mut conn)?;
+            },
+            2 => {
+                let city = City::new("Seoul", 1);
+                city.add_city(&mut conn)?;
+            },
+            3 => {
+                let country = Country::new("South Korea", "SK");
+                country.add_country(&mut conn)?;
+            },
+            4 => {
+                isEnd = true;
+            },
+            _ => {
+                println!("Invalid choice");
+            }
+        }
+    }
+
 
     let country = Country::new("South Korea", "SK");
     country.add_country(&mut conn)?;
     Ok(())
+
 }
--- src/models/user.rs
+++ src/models/user.rs
@@ -11,37 +11,77 @@ pub struct User {
 }
 
 impl User {
+
+    // Sprawdzenie, czy użytkownik istnieje w bazie
+    pub fn user_exists(conn: &mut PooledConn, login: &str) -> bool {
+        let exists: Option<String> = conn.exec_first(
+            "SELECT login FROM user WHERE login = :login",
+            params! {
+                "login" => login,
+            },
+        ).unwrap_or(None); 
+    
+        exists.is_some() // Zwróć true, jeśli użytkownik istnieje
+    }
+
+    // Konstruktor nowego użytkownika
     pub fn new(login: &str, password: &str, status: &str) -> Self {
         User {
-            user_id: 0,  // Autoinkrementacja, zostanie nadpisane po dodaniu do bazy
+            user_id: 0,
             login: login.to_owned(),
             password: password.to_owned(),
             status: status.to_owned(),
         }
     }
 
+    // Dodanie użytkownika, tylko jeśli nie istnieje
     pub fn add_user(&self, conn: &mut PooledConn) -> Result<(), mysql::Error> {
-        conn.exec_drop(
-            r"INSERT INTO user (login, password, status)
-            VALUES (:login, :password, :status)",
-            params! {
-                "login" => &self.login,
-                "password" => &self.password,
-                "status" => &self.status,
-            }
-        )?;
+        if Self::user_exists(conn, &self.login) == false {
+            conn.exec_drop(
+                r"INSERT INTO user (login, password, status)
+                VALUES (:login, :password, :status)",
+                params! {
+                    "login" => &self.login,
+                    "password" => &self.password,
+                    "status" => &self.status,
+                }
+            )?;
+            println!("User '{}' added successfully!", &self.login);
+        } else {
+            println!("User '{}' already exists!", &self.login);
+        }
         Ok(())
     }
 
+    // Usuwanie użytkownika
     pub fn remove_user(&self, conn: &mut PooledConn) -> Result<(), mysql::Error> {
-        conn.exec_drop(
-            r"DELETE FROM user WHERE login = :login AND password = :password AND status = :status",
-            params! {
-                "login" => &self.login,
-                "password" => &self.password,
-                "status" => &self.status,
-            }
-        )?;
+        if Self::user_exists(conn, &self.login) {
+            conn.exec_drop(
+                r"DELETE FROM user WHERE login = :login",
+                params! {
+                    "login" => &self.login,
+                }
+            )?;
+            println!("User '{}' removed.", &self.login);
+        } else {
+            println!("User '{}' does not exist.", &self.login);
+        }
+        Ok(())
+    }
+
+    // Promowanie użytkownika do admina
+    pub fn promote_user(&self, conn: &mut PooledConn) -> Result<(), mysql::Error> {
+        if Self::user_exists(conn, &self.login) {
+            conn.exec_drop(
+                r"UPDATE user SET status = 'admin' WHERE login = :login",
+                params! {
+                    "login" => &self.login,
+                }
+            )?;
+            println!("User '{}' promoted to admin.", &self.login);
+        } else {
+            println!("User '{}' does not exist.", &self.login);
+        }
         Ok(())
     }
 }
