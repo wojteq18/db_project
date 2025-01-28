e2dc5f95264d7804d38da3b40a88e3d2363f2d18
Wojtaz <279730@student.pwr.edu.pl>
2025-01-28 15:57:57 +0100

    Funkcje admina dzialaja w 60%

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,18 @@ use models::country::Country;
 use models::hotel::Hotel;
 use bcrypt::{verify};
 use possibilities::user_actions::{UserActions, NormalUser};
+use possibilities::admin_actions::{AdminActions, Admin};
 use auth::{Auth, AuthService};
 
 fn main() -> Result<()> {
     let pool = connect_to_db()?;
     let mut conn = pool.get_conn()?;
-    let mut is_logged: bool = false;
-    let mut is_admin: bool = false;
+    let mut is_logged = false;
+    let mut is_admin = false;
 
-    while is_logged == false {
-        println!("To use application, you have to be logged");
-        println!("Choose number of action:");
+    while !is_logged {
+        println!("To use the application, you have to be logged in");
+        println!("Choose the number of an action:");
         println!("1. Log in");
         println!("2. Register");
 
@@ -34,20 +35,21 @@ fn main() -> Result<()> {
             1 => {
                 let auth_status = AuthService::log_in(&mut conn);
                 is_admin = auth_status.is_admin;
-                is_logged = auth_status.is_logged
+                is_logged = auth_status.is_logged;
             }
             2 => AuthService::register(&mut conn),
-            _ => println!("Please type a number from 1 to 2")
+            _ => println!("Please type a number from 1 to 2"),
         }
     }
 
-    if is_admin == false {
-        let mut is_ended: bool = false;
+    let mut is_ended = false;
 
-        while is_ended == false {
-            println!("Choose number of action: ");
-            println!("1. Show connection beetwen cities");
-            println!("2. Show hotels in city");
+    while !is_ended {
+        if !is_admin {
+            // Opcje dla zwykłego użytkownika
+            println!("Choose the number of an action:");
+            println!("1. Show connections between cities");
+            println!("2. Show hotels in a city");
             println!("3. Log out");
 
             let mut action = String::new();
@@ -59,10 +61,40 @@ fn main() -> Result<()> {
                 2 => NormalUser::show_hotels(&mut conn),
                 3 => {
                     is_ended = true;
+                    println!("Logged out successfully.");
                 }
-                _ => println!("Please type number from 1 to 3")
+                _ => println!("Please type a number from 1 to 3"),
+            }
+        } else {
+            // Opcje dla administratora
+            println!("Choose the number of an action:");
+            println!("1. Add city");
+            println!("2. Remove city");
+            println!("3. Add country");
+            println!("4. Remove country");
+            println!("5. Add hotel");
+            println!("6. Remove hotel");
+            println!("7. Log out");
+
+            let mut action = String::new();
+            std::io::stdin().read_line(&mut action).expect("Failed to read line");
+            let action: i32 = action.trim().parse().expect("Please type a number!");
+
+            match action {
+                1 => Admin::add_city(&mut conn),
+                2 => Admin::remove_city(&mut conn),
+                3 => Admin::add_country(&mut conn),
+                4 => Admin::remove_country(&mut conn),
+                5 => Admin::add_hotel(&mut conn),
+                6 => Admin::remove_hotel(&mut conn),
+                7 => {
+                    is_ended = true;
+                    println!("Logged out successfully.");
+                }
+                _ => println!("Please type a number from 1 to 7"),
             }
         }
     }
+
     Ok(())
 }
--- src/possibilities/admin_actions.rs
+++ src/possibilities/admin_actions.rs
@@ -1,27 +1,27 @@
 use mysql::*;
 use mysql::prelude::*;
-use crate::models::{city::{self, City}, city_transport::City_transport, country::Country, hotel::Hotel};
+use crate::models::{city::{self, City}, city_transport::City_transport, country::{self, Country}, hotel::Hotel};
+use chrono::NaiveDateTime;
+
 
 pub trait AdminActions {
     fn add_city(conn: &mut PooledConn);
     fn add_country(conn: &mut PooledConn);
     fn add_hotel(conn: &mut PooledConn);
     fn add_city_transport(conn: &mut PooledConn);
+    fn remove_city(conn: &mut PooledConn);
+    fn remove_country(conn: &mut PooledConn);
+    fn remove_hotel(conn: &mut PooledConn);
+    //TODO add remove fuction
 }
 
 pub struct Admin;
 
 impl AdminActions for Admin {
     fn add_city(conn: &mut PooledConn) {
-        println!("Type country name: ");
-        let mut country_name = String::new();
-        std::io::stdin().read_line(&mut country_name).expect("Failed to read line");
-        let country_name = country_name.trim().to_string();
-
-        println!("Type city name: ");
-        let mut city_name = String::new();
-        std::io::stdin().read_line(&mut city_name).expect("Failed to read line");
-        let city_name = city_name.trim().to_string();
+        
+        let city_name = get_city();
+        let country_name = get_country();
 
         if City::city_exists(conn, &country_name, &city_name) {
             println!("City '{}' already exists!", city_name);
@@ -31,16 +31,9 @@ impl AdminActions for Admin {
         }
     }
     fn add_country(conn: &mut PooledConn) {
-        println!("Type country name: ");
-        let mut country_name = String::new();
-        std::io::stdin().read_line(&mut country_name).expect("Failed to read line");
-        let country_name = country_name.trim().to_string();
-
-        println!("Type country code: ");
-        let mut country_code = String::new();
-        std::io::stdin().read_line(&mut country_code).expect("Failed to read line");
-        let country_code = country_code.trim().to_string();
-
+        let country_name = get_country();
+        let country_code = get_country_code();
+    
         if Country::country_exists(conn, &country_name) {
             println!("Country '{}' already exists!", country_name);
         } else {
@@ -49,20 +42,9 @@ impl AdminActions for Admin {
         }
     }
     fn add_hotel(conn: &mut PooledConn) {
-        println!("Type hotel name: ");
-        let mut hotel_name = String::new();
-        std::io::stdin().read_line(&mut hotel_name).expect("Failed to read line");
-        let hotel_name = hotel_name.trim().to_string();
-
-        println!("Type city name: ");
-        let mut city_name = String::new();
-        std::io::stdin().read_line(&mut city_name).expect("Failed to read line");
-        let city_name = city_name.trim().to_string();
-
-        println!("Type hotel rating: ");
-        let mut hotel_rating = String::new();
-        std::io::stdin().read_line(&mut hotel_rating).expect("Failed to read line");
-        let hotel_rating: i32 = hotel_rating.trim().parse().expect("Please type a number!");
+        let hotel_name = get_hotel();
+        let city_name = get_city();
+        let hotel_rating = get_hotel_rating();
 
         if Hotel::hotel_exists(conn, &hotel_name, &city_name) {
             println!("Hotel '{}' already exists!", hotel_name);
@@ -75,6 +57,149 @@ impl AdminActions for Admin {
         }
     }
     fn add_city_transport(conn: &mut PooledConn) {
-        //TODO
+        let city_departure_name = get_city();
+        let city_arrival_name = get_city();
+        let transport_name = get_transport();
+        let price = get_price();
+        let departure_time = get_date();
+        let arrival_time = get_date();
+    
+        // Sprawdzenie, czy transport już istnieje
+        if City_transport::city_transport_exists(conn, &city_departure_name, &city_arrival_name) {
+            println!(
+                "City transport from '{}' to '{}' by '{}' already exists!",
+                city_departure_name, city_arrival_name, transport_name
+            );
+            } else {
+            // Dodanie nowego transportu
+            City_transport::add_city_transport(
+                conn,
+                &city_departure_name,
+                &city_arrival_name,
+                price,
+                departure_time,
+                arrival_time,
+                &transport_name,
+            ).expect("Failed to add city transport"); 
+        }
     }
+    
+
+    fn remove_city(conn: &mut PooledConn) {
+        
+        let city_name = get_city();
+        let country_name = get_country();
+
+        if City::city_exists(conn, &city_name, &country_name) {
+            let city = City::new(&city_name, &country_name);
+            city.remove_city(conn).expect("Failed to remove city");
+        }
+    }
+
+    fn remove_country(conn: &mut PooledConn) {
+        let country_name = get_country();
+        let country_code = get_country_code();
+
+        if Country::country_exists(conn, &country_name) {
+            let country = Country::new(&country_name, &country_code);
+            country.remove_country(conn).expect("Failed to remove country");
+        } 
+    }
+
+    fn remove_hotel(conn: &mut PooledConn) {
+        let hotel_name = get_hotel();
+        let city_name = get_city();
+
+        if Hotel::hotel_exists(conn, &hotel_name, &city_name) {
+            Hotel::remove_hotel(conn, &hotel_name, &city_name).expect("Failed to remove hotel");
+        } else {
+            println!("Hotel '{}' does not exist!", hotel_name);
+        }
+    }
+}
+
+fn get_city() -> String { //funkcja pobierająca nazwe miasta
+
+    println!("Type city name: ");
+    let mut city_name = String::new();
+    std::io::stdin().read_line(&mut city_name).expect("Failed to read line");
+    let city_name = city_name.trim().to_string();
+
+    return city_name
+}
+
+fn get_country() -> String { //funkcja pobierająca nazwe kraju
+
+    println!("Type country name: ");
+    let mut country_name = String::new();
+    std::io::stdin().read_line(&mut country_name).expect("Failed to read line");
+    let country_name = country_name.trim().to_string();
+
+    return country_name
+}
+
+fn get_country_code() -> String { //funkcja pobierająca kod kraju
+
+    println!("Type country code: ");
+    let mut country_code = String::new();
+    std::io::stdin().read_line(&mut country_code).expect("Failed to read line");
+    let country_code = country_code.trim().to_string();
+
+    return country_code
+}
+
+fn get_hotel() -> String { //funkcja pobierająca nazwe hotelu
+
+    println!("Type hotel name: ");
+    let mut hotel_name = String::new();
+    std::io::stdin().read_line(&mut hotel_name).expect("Failed to read line");
+    let hotel_name = hotel_name.trim().to_string();
+
+    return hotel_name
+
+}
+
+fn get_hotel_rating() -> i32 { //funkcja pobierająca ocene hotelu
+
+    println!("Type hotel rating: ");
+    let mut hotel_rating = String::new();
+    std::io::stdin().read_line(&mut hotel_rating).expect("Failed to read line");
+    let hotel_rating: i32 = hotel_rating.trim().parse().expect("Please type a number!");
+
+    return hotel_rating
+
+}
+
+fn get_transport() -> String { //funkcja pobierająca nazwe transportu
+
+    println!("Type transport name: ");
+    let mut transport_name = String::new();
+    std::io::stdin().read_line(&mut transport_name).expect("Failed to read line");
+    let transport_name = transport_name.trim().to_string();
+
+    return transport_name
+
+}
+
+fn get_price() -> f64 { //funkcja pobierająca cene transportu
+
+    println!("Type transport price: ");
+    let mut price = String::new();
+    std::io::stdin().read_line(&mut price).expect("Failed to read line");
+    let price: f64 = price.trim().parse().expect("Please type a valid number!");
+
+    return price
+
+}
+
+fn get_date() -> NaiveDateTime { //funkcja pobierająca date
+
+    println!("Type date (YYYY-MM-DD HH:MM:SS): ");
+    let mut date = String::new();
+    std::io::stdin().read_line(&mut date).expect("Failed to read line");
+    let date = NaiveDateTime::parse_from_str(date.trim(), "%Y-%m-%d %H:%M:%S")
+        .expect("Invalid date format!");
+
+    return date
+
 }
